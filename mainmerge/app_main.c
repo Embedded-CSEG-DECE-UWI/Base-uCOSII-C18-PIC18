adee3015c264ce27313fa306495cac460673330a
Administrator <Administrator@192.168.25.35>
2018-08-24 18:55:36 -0300

    got those files in there!

---
+++ mainmerge/app_main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   app_main.c
+ * Author: Administrator
+ *
+ * Created on August 24, 2018, 6:47 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
